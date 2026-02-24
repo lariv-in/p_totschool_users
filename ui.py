5db7b13e05eccfc37f48cc7ca9cf60790a7c73ad
Raj Tagore <rajtagore780@gmail.com>
2026-02-24 22:26:02 +0000

    --

--- ui.py
+++ ui.py
@@ -1,22 +1,23 @@
-from lariv.registry import UIRegistry, ComponentRegistry
+from lariv.registry import UIRegistry
+from components import *  # noqa
 
 
 def add_hello_message(c):
     # Insert the message before the apps grid
     c.children.insert(
         0,
-        ComponentRegistry.get("column")(
+        Column(
             uid="apps-top-column",
             children=[
-                ComponentRegistry.get("row")(
+                Row(
                     uid="apps-hello-row",
                     classes="flex items-end gap-2 text-3xl font-bold text-base-content mb-2 mt-4 max-w-5xl mx-auto px-6",
                     children=[
-                        ComponentRegistry.get("text_field")(
+                        TextField(
                             uid="apps-hello-text",
                             static_value="Hello, ",
                         ),
-                        ComponentRegistry.get("text_field")(
+                        TextField(
                             uid="apps-hello-name",
                             key="request.user.name",
                         ),
@@ -28,8 +29,8 @@ def add_hello_message(c):
     return c
 
 
-# Patch the AppsPage to include the dynamic Hello message
-UIRegistry.get("lariv.AppsPage").patch(
-    uid="apps-scaffold",
-    fn=add_hello_message,
-)
+# # Patch the AppsPage to include the dynamic Hello message
+# UIRegistry.get("lariv.AppsPage").patch(
+#     uid="apps-scaffold",
+#     fn=add_hello_message,
+# )
