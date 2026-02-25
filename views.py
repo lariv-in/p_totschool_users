e763991abb13e916abf46246cf8d47c5a386b287
Sandeep Kumar <sandeepqmar05@gmail.com>
2026-02-25 23:58:34 +0530

    --

--- views.py
+++ views.py
@@ -2,14 +2,14 @@ from lariv.registry import ViewRegistry
 from lariv.views import AppsPage
 
 
-# @ViewRegistry.register("lariv.AppsPage")
+@ViewRegistry.register("lariv.AppsPage")
 class PatchedAppsPage(AppsPage):
     def prepare_data(self, request, **kwargs):
         data = super().prepare_data(request, **kwargs)
         apps_dict = data.get("apps", {})
 
         # Stop 'users' and 'preferences' apps from displaying for non-superusers
-        if not (request.user.is_superuser or request.user.role in ["totschool_admin"]):
+        if not (request.user.is_superuser or request.user.role.name in ["totschool_admin"]):
             apps_dict.pop("users", None)
             apps_dict.pop("preferences", None)
 
