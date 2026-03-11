6261638f97246b0efe57459063e2250511ffc8e8
Raj Tagore <rajtagore780@gmail.com>
2026-03-11 21:35:46 +0000

    --

--- apps.py
+++ apps.py
@@ -1,23 +1,22 @@
 from django.apps import AppConfig
-from lariv.registry import ConfigRegistry
+from lariv.registry import MetaRegistry
 
 
-@ConfigRegistry.register("p_totschool_users")
 class TotschoolUsersConfig(AppConfig):
     name = "p_totschool_users"
-    p_type = "plugin"
-    verbose_name = "Users"
-    url_prefix = "users"
-    icon = "user"
 
     def ready(self):
-        # Patch the core users app to be visible to totschool_admin
-        try:
-            core_users_class = ConfigRegistry.get("users")
-            core_users_class.roles = getattr(core_users_class, "roles", []) + [
-                "totschool_admin"
-            ]
-        except ValueError:
-            pass  # Core users app not registered
-
         from . import ui, views  # noqa: F401
+
+        UsersMeta = MetaRegistry.get("users")
+
+        @MetaRegistry.register("users")
+        class NewUsersMeta(UsersMeta):
+            roles = ["totschool_admin"]
+
+
+@MetaRegistry.register("p_totschool_users")
+class TotschoolUsersMeta:
+    p_type = "plugin"
+    verbose_name = "Totschool Users"
+    url_prefix = "totschool_users"
